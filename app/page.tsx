7912d6c94652419dafd5a7be46741dbfb21bbf2f
viettv1 <viettv1@smartosc.com>
2024-11-17 18:17:53 +0700

    update video handling

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ import ThankYou from "@/components/thankyou";
 import WeddingGift from "@/components/WeddingGift";
 import QR from "@/components/WeddingGift/QR";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
   const [isPlaying, setIsPlaying] = useState(false);
@@ -20,10 +20,32 @@ export default function Home() {
   const [isOverflowAuto, setIsOverflowAuto] = useState(true);
   const [content, setContent] = useState<any>(null);
 
+  const userInteracted = useRef(false);
+
   useEffect(() => {
-    setTimeout(() => {
+    const handleUserInteraction = (e: any) => {
+      if (e?.target?.id === "playMucsic-btn") {
+        userInteracted.current = true;
+        return;
+      }
+      if (userInteracted.current) return;
+      userInteracted.current = true;
+
+      document.removeEventListener("click", handleUserInteraction);
+      document.removeEventListener("touchstart", handleUserInteraction);
+      document.removeEventListener("keydown", handleUserInteraction);
+
       toggleMusic();
-    }, 2000);
+    };
+    document.addEventListener("click", handleUserInteraction);
+    document.addEventListener("touchstart", handleUserInteraction);
+    document.addEventListener("keydown", handleUserInteraction);
+
+    return () => {
+      document.removeEventListener("click", handleUserInteraction);
+      document.removeEventListener("touchstart", handleUserInteraction);
+      document.removeEventListener("keydown", handleUserInteraction);
+    };
   }, []);
 
   const toggleMusic = () => {
@@ -69,7 +91,7 @@ export default function Home() {
         <audio
           id="musicPlayer"
           loop
-          autoPlay
+          // autoPlay
           src="https://procare.one/wp-content/uploads/2024/05/Shane-Filan-Beautiful-In-White.mp3"
         ></audio>
         <Section1 />
@@ -87,9 +109,11 @@ export default function Home() {
       <div
         id="playMucsic-btn"
         className={`tdk-music cursor-pointer ${isPlaying ? "playing" : ""}`}
-        onClick={toggleMusic}
+        onClick={(e) => {
+          toggleMusic();
+        }}
       >
-        <div className="tdk-bg-player"></div>
+        <div style={{ pointerEvents: "none" }} className="tdk-bg-player"></div>
         {/* <div className="tdk-player-text">Phát Nhạc</div> */}
 
         {isPlaying ? (
@@ -99,6 +123,7 @@ export default function Home() {
             width="26"
             height="26"
             viewBox="0 0 26 26"
+            style={{ pointerEvents: "none" }}
           >
             <path d="M 7 5 C 6.449 5 6 5.449 6 6 L 6 20 C 6 20.551 6.449 21 7 21 L 10 21 C 10.551 21 11 20.551 11 20 L 11 6 C 11 5.449 10.551 5 10 5 L 7 5 z M 16 5 C 15.449 5 15 5.449 15 6 L 15 20 C 15 20.551 15.449 21 16 21 L 19 21 C 19.551 21 20 20.551 20 20 L 20 6 C 20 5.449 19.551 5 19 5 L 16 5 z"></path>
           </svg>
@@ -109,6 +134,7 @@ export default function Home() {
             width="26"
             height="26"
             viewBox="0 0 26 26"
+            style={{ pointerEvents: "none" }}
           >
             <path d="M20.208,11.857L6.902,5.26C6.499,5.057,6.018,5.079,5.634,5.312c-0.386,0.232-0.619,0.646-0.619,1.09V19.6 c0,0.443,0.233,0.856,0.619,1.089c0.208,0.126,0.444,0.19,0.683,0.19c0.201,0,0.401-0.046,0.586-0.138l13.306-6.599 c0.438-0.218,0.716-0.658,0.716-1.143S20.646,12.075,20.208,11.857z"></path>
           </svg>
