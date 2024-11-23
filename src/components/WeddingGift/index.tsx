722a33420f4e7dc42a94abcc88787e8048a9c00b
viettv1 <viettv1@smartosc.com>
2024-11-23 13:16:52 +0700

    add arrow

--- src/components/WeddingGift/index.tsx
+++ src/components/WeddingGift/index.tsx
@@ -20,7 +20,10 @@ const WeddingGift = () => {
             đám cưới của chúng tôi.
           </div>
         </AnimateOnScroll>
-        <div className="flex justify-center mt-2 md:mt-5">
+        <div className="flex justify-center mt-2 md:mt-5 relative">
+          {/* <div className="absolute right-[140px] top-[120px]">
+            <img src="/images/bg/arrow.png" className="w-[170px] h-[120px]  "/>
+          </div> */}
           <div className="relative animate-[tada_2s_infinite_1s]">
             <div className={style.box}>
               <Gift className="w-[70px] h-[70px] md:w-[140px] md:h-[117px]" />
@@ -29,6 +32,10 @@ const WeddingGift = () => {
                 className="absolute w-full h-full top-0 left-0"
               ></div>
             </div>
+            <img
+              src="/images/bg/arrow.png"
+              className="w-[80px] h-[60px] md:w-[170px] md:h-[120px] absolute top-0 right-0 translate-x-[120%] translate-y-[100%]"
+            />
           </div>
         </div>
       </div>
